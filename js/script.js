3ee07590dfc82b84a587c79f8dd2ee331ba16cc0
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2018-03-01 15:45:33 -0300

    Ajustes

--- js/script.js
+++ js/script.js
@@ -44,7 +44,6 @@ arrow[0].onclick = function () {
     if (index < 0) {
         index = 0;
     }
-    console.log(index);
 
 
     for (i = 0; i < div.length; i++) {
@@ -55,10 +54,7 @@ arrow[0].onclick = function () {
 }
 
 arrow[1].onclick = function () {
-    let div = document.getElementsByClassName("partners-div");
     index += 1;
-    console.log("Index : " + index);
-    console.log("Quantidade de DIVS :" + div.length);
     
     if (index > div.length - 1) {
         index = div.length - 1;
@@ -75,60 +71,84 @@ arrow[1].onclick = function () {
 
 }
 
-//Pega o container das setas da area das cores do produto
-let innerProducts = document.getElementsByClassName("inner-products"); 
-console.log(innerProducts.children[0]);
+//Essa seção da interatividade nas abas dos produto
+let releases1Arrow = document.querySelectorAll(".releases1 .arrow");
+let products1 = document.getElementsByClassName("products1");
+let productIndex = 0;
 
-/*
-//Pega as setas da area de parceiros
-let arrow = document.getElementsByClassName("arrow");
-//Pega as divs
-let div = document.getElementsByClassName("partners-div");
-//Posição(DIV) atual
-let index = 0;
-
-//limpa os campos das divs e deixa so a primeira
-for (i = 0; i < div.length; i++) {
-    div[i].style.display = "none";
+for(i = 0; i < products1.length; i++){
+    products1[i].style.display = "none";
 }
 
-div[0].style.display = "flex";
+products1[0].style.display = "flex";
 
-//Adiciona um evento em ambas para mudar de div
-arrow[0].onclick = function () {
-    index -= 1;
+//Adiciona um evento clique em ambas para mudar de div
+releases1Arrow[0].onclick = function(){
+     productIndex -= 1;
 
-    if (index < 0) {
-        index = 0;
+    if (productIndex < 0) {
+        productIndex = 0;
     }
-    console.log(index);
 
-
-    for (i = 0; i < div.length; i++) {
-        div[i].style.display = "none";
+    for (i = 0; i < products1.length; i++) {
+        products1[i].style.display = "none";
     }
-    div[index].style.display = "flex";
+    products1[productIndex].style.display = "flex";
 
 }
 
-arrow[1].onclick = function () {
-    let div = document.getElementsByClassName("partners-div");
-    index += 1;
-    console.log("Index : " + index);
-    console.log("Quantidade de DIVS :" + div.length);
-    
-    if (index > div.length - 1) {
-        index = div.length - 1;
+releases1Arrow[1].onclick = function(){
+    console.log(products1[6]);
+    productIndex += 1;
+    if (productIndex > products1.length - 1) {
+        productIndex = products1.length - 1;
     }
-    
-
-
-
-    for (i = 0; i < div.length; i++) {
-        div[i].style.display = "none";
+    console.log(productIndex);
+    for (i = 0; i < products1.length; i++) {
+        products1[i].style.display = "none";
     }
-    div[index].style.display = "flex";
-
+    products1[productIndex].style.display = "flex";
 
 }
-*/
+
+let releases2Arrow = document.querySelectorAll(".releases2 .arrow");
+let products2 = document.getElementsByClassName("products2");
+let product2Index = 0;
+console.log(releases2Arrow);
+
+for(i = 0; i < products2.length; i++){
+    products2[i].style.display = "none";
+}
+
+products2[0].style.display = "flex";
+
+releases2Arrow[0].onclick = function(){
+     product2Index -= 1;
+
+    if (product2Index < 0) {
+        product2Index = 0;
+    }
+
+    for (i = 0; i < products2.length; i++) {
+        products2[i].style.display = "none";
+    }
+    products2[product2Index].style.display = "flex";
+    console.log("click");
+}
+
+releases2Arrow[1].onclick = function(){
+    console.log("clicou");
+    product2Index += 1;
+    if (product2Index > products2.length - 1) {
+        product2Index = products2.length - 1;
+    }
+    console.log("checou");
+
+    for (i = 0; i < products2.length; i++) {
+        products2[i].style.display = "none";
+    }
+    console.log("limpou");
+    products2[product2Index].style.display = "flex";
+    console.log("mostrou");
+
+}
