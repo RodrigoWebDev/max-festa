b1ea4c3e955183b73d2bda35c665980cca50e9bf
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2018-03-02 00:26:17 -0300

    Dropdowns finalizados

--- js/script.js
+++ js/script.js
@@ -55,11 +55,11 @@ arrow[0].onclick = function () {
 
 arrow[1].onclick = function () {
     index += 1;
-    
+
     if (index > div.length - 1) {
         index = 0;
     }
-    
+
 
 
 
@@ -76,15 +76,15 @@ let releases1Arrow = document.querySelectorAll(".releases1 .arrow");
 let products1 = document.getElementsByClassName("products1");
 let productIndex = 0;
 
-for(i = 0; i < products1.length; i++){
+for (i = 0; i < products1.length; i++) {
     products1[i].style.display = "none";
 }
 
 products1[0].style.display = "flex";
 
 //Adiciona um evento clique em ambas para mudar de div
-releases1Arrow[0].onclick = function(){
-     productIndex -= 1;
+releases1Arrow[0].onclick = function () {
+    productIndex -= 1;
 
     if (productIndex < 0) {
         productIndex = products1.length - 1;
@@ -97,7 +97,7 @@ releases1Arrow[0].onclick = function(){
 
 }
 
-releases1Arrow[1].onclick = function(){
+releases1Arrow[1].onclick = function () {
     console.log(products1[6]);
     productIndex += 1;
     if (productIndex > products1.length - 1) {
@@ -114,16 +114,15 @@ releases1Arrow[1].onclick = function(){
 let releases2Arrow = document.querySelectorAll(".releases2 .arrow");
 let products2 = document.getElementsByClassName("products2");
 let product2Index = 0;
-console.log(releases2Arrow);
 
-for(i = 0; i < products2.length; i++){
+for (i = 0; i < products2.length; i++) {
     products2[i].style.display = "none";
 }
 
 products2[0].style.display = "flex";
 
-releases2Arrow[0].onclick = function(){
-     product2Index -= 1;
+releases2Arrow[0].onclick = function () {
+    product2Index -= 1;
 
     if (product2Index < 0) {
         product2Index = products2.length - 1;
@@ -133,22 +132,73 @@ releases2Arrow[0].onclick = function(){
         products2[i].style.display = "none";
     }
     products2[product2Index].style.display = "flex";
-    console.log("click");
 }
 
-releases2Arrow[1].onclick = function(){
-    console.log("clicou");
+releases2Arrow[1].onclick = function () {
     product2Index += 1;
     if (product2Index > products2.length - 1) {
         product2Index = 0;
     }
-    console.log("checou");
 
     for (i = 0; i < products2.length; i++) {
         products2[i].style.display = "none";
     }
-    console.log("limpou");
     products2[product2Index].style.display = "flex";
-    console.log("mostrou");
 
+}
+
+//Drop down login e sacola de compras
+let user = document.getElementsByClassName("hover")[0];
+let login = document.getElementsByClassName("login")[0];
+let basket = document.getElementsByClassName("hover")[1];
+let shop = document.getElementsByClassName("shop")[0];
+
+user.onmouseover = function () {
+    login.style.display = "block";
+}
+login.onmouseover = function () {
+    this.style.display = "block";
+}
+
+user.onmouseout = function () {
+    login.style.display = "none";
+}
+login.onmouseout = function () {
+    this.style.display = "none";
+}
+
+shop.onmouseover = function () {
+    this.style.display = "block";
+}
+basket.onmouseover = function () {
+    shop.style.display = "block";
+}
+
+shop.onmouseout = function () {
+    this.style.display = "none";
+}
+basket.onmouseout = function () {
+    shop.style.display = "none";
+}
+
+let dropdownNav = document.getElementsByClassName("dropdown-nav")[0];
+let menu = document.querySelectorAll("nav ul div");
+dropdownNav.style.display = "none";
+console.log(menu);
+
+for (i = 0; i < menu.length; i++) {
+    menu[i].onmouseover = function () {
+        dropdownNav.style.display = "flex";
+    }
+    menu[i].onmouseout = function () {
+        dropdownNav.style.display = "none";
+    }
+}
+
+dropdownNav.onmouseover = function () {
+    dropdownNav.style.display = "flex";
+}
+
+dropdownNav.onmouseout = function () {
+    dropdownNav.style.display = "none";
 }
